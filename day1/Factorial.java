b01609bb6fde76836dc71c5bb3763c84f70f8768
GowthamiSS <96673188+GowthamiSS@users.noreply.github.com>
2021-12-26 10:07:25 +0530

    Add files via upload

---
+++ day1/Factorial.java
@@ -0,0 +1,14 @@
+package week1.day1;
+public class Factorial {
+	public static void main(String[] args) {
+		// Factorial of the number 
+		int input = 12;
+		int fact = 1;
+	    for (int i = 1 ; i <= input ; i++ )
+	    {
+	            fact = fact*i;
+	    }
+	         System.out.println(fact);
+	      
+	   }
+	}
